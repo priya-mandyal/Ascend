d9b20e0935552e95b8a9b18db16447786609c4f0
priya-mandyal <priya.mandyal98@gmail.com>
2026-02-22 20:59:56 -0500

    chore: base structure

---
+++ src/main/java/com/gymtracker/ascend/AscendApplication.java
@@ -0,0 +1,13 @@
+package com.gymtracker.ascend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AscendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AscendApplication.class, args);
+	}
+
+}
